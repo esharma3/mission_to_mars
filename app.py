1aca1319b5adcb8b2dccc4268fd55d8a62aa1746
Ekta Sharma <ekta.sharma3@gmail.com>
2020-03-03 00:38:35 -0600

    added comments

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import pymongo
 
 app = Flask(__name__)
 
+# setting up MongoDB connection and creating a new database 
 CONN = "mongodb://localhost:27017"
 client = pymongo.MongoClient(CONN)
 db = client.mars_db
 
-
+# home(/) route retrieves the information from the MongoDB database and displays it on the home page.
 @app.route("/")
 def home_page():
 	result = db.mars_collection.find_one()
 	return render_template("index.html", mars=result)
 
+# scrape route scrapes information related to 'Mission to Mars' from various webpages and stores them to the MongoDB database.
 @app.route("/scrape")
 def scrape_data():
 	mars_dict = scrape()
@@ -21,8 +23,7 @@ def scrape_data():
 	return redirect("/", code=302)
 
 
-
-#  main ----------------------------------------------------------------------
+#  main -------------------------------------------------------------
 
 if __name__ == "__main__":
 	app.run(debug=True)
